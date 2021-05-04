17a9b0b9a8a8df292098a5432c65cabcf084e3b5
guymanpersonboy <chris.carrasco@att.net>
2021-05-04 00:02:45 -0500

    goodnight

--- pipe/psim.c
+++ pipe/psim.c
@@ -1036,6 +1036,7 @@ void do_stall_check()
             // normal case
             decode_state->op = pipe_cntl("ID", false, true);
             execute_state->op = pipe_cntl("EX", false, true);
+            // vala is valp i.e. fall through
             fetch_input->predPC = execute_output->vala;
         }
         break;
